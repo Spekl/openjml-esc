c923289c7dfad2c4bd9cb859f43cb6d4b549f2f5
John L. Singleton <jsinglet@gmail.com>
2015-08-08 11:34:22 -0400

    Creation of package

--- check.clj
+++ check.clj
@@ -1,15 +1,32 @@
 (ns spekl-package-manager.check
  (:require [clojure.java.io :as io]
-            [clojure.tools.logging :as log]))
+            [clojure.tools.logging :as log]
+            [spekl-package-manager.util :as util]
+            [spekl-package-manager.constants :as constants]
+            [spekl-package-manager.download :as download]
+            [spekl-package-manager.package :as package]
+            [clojure.core.reducers :as r]
+            [org.satta.glob :as glob]
+            [clojure.string :as string]
+            [clojure.java.shell :as shell]
+            )
 
-;;
-;; All checks must at least define a "default" check and can optionally define any number of named checks.
-;;
+  )
+
+(defn configure-solver [path]
+  (spit
+   "openjml.properties"
+   (str
+    "openjml.defaultProver=z3_4_3\n"
+    "openjml.prover.z3_4_3=" (string/replace path "\\" "\\\\") "\n" ;; needed to support windows
+    )))
+
 (defcheck default
-  ;; Printing a log message...
-  (log/info  "A message")
+  ;; create a solver configuration
+  (log/info  "Configuring solver for Z3...")
+  (configure-solver (resolve-path "z3" "z3-4.3.0-x86/bin/z3.exe"))
 
-  ;; running a check
-  (log/info  "Running a check.")  
-  (run "cmd" "arg1" "${installed-package:asset.jar}" "-esc" *project-files-string* ))
+  ;; run the check
+  (log/info  "Running OpenJML in ESC Mode...")  
+  (run "java" "-jar" "${openjml:openjml.jar}" "-esc" *project-files-string* ))
 
